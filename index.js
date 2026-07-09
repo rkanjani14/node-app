39c60aa83776650792550b602d45394a677247c8
Rakesh Anjani <rkanjani14@gmail.com>
2026-07-09 14:50:30 +0530

    added dockerfile

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Node.js API!' });
 });
 
+app.get('/api/goodbye', (req, res) => {
+  res.json({ message: 'Goodbye from Node.js API!' });
+}
+
 app.post('/api/echo', (req, res) => {
   res.json({ received: req.body });
 });
